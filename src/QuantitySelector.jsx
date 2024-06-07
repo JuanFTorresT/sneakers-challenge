233a6ad30d55c0ac7ae53633c5a74d578a313fdd
Felipe Torres <juanftorrest@gmail.com>
2024-06-07 11:27:25 -0500

    se agregó la funcionalidad del selector de cantidad

--- src/QuantitySelector.jsx
+++ src/QuantitySelector.jsx
@@ -1,10 +1,22 @@
+import { useState } from "react";
+
 export default function QuantitySelector() {
+    let [selectedQuantity, setSelectedQuantity] = useState(1);
+    const sumar = () => {
+        setSelectedQuantity(selectedQuantity+1)
+    }
+
+    const restar = () => {
+        const condition = selectedQuantity > 1 ? setSelectedQuantity(selectedQuantity-1) : setSelectedQuantity(1)
+        
+    }
+
     return(
         <>
             <div className="quantity-selector bg-slate-100 p-2 rounded flex justify-between">
-                <button className="text-orange-500 font-bold text-2xl px-4">-</button>
-                <input className="bg-transparent text-center font-bold" type="number" placeholder="0" />
-                <button className="text-orange-500 font-bold text-2xl px-4">+</button>
+                <button onClick={restar} className="text-orange-500 font-bold text-2xl px-4">-</button>
+                <input className="bg-transparent text-center font-bold" type="number" value={selectedQuantity} />
+                <button onClick={sumar} className="text-orange-500 font-bold text-2xl px-4">+</button>
             </div>
         </>
     );
