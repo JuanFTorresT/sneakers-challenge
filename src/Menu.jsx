0c79fbb944097fcdd02549141c908cd88b6bf52e
Felipe Torres <[juanftorrest@gmail.com]>
2024-06-08 07:59:39 -0500

    se corrigío la funcionalidad del menu

--- src/Menu.jsx
+++ src/Menu.jsx
@@ -1,22 +1,24 @@
-import logo from '../public/assets/productImages/logo.svg'
+import logo from '/assets/productImages/logo.svg'
 import Carrito from './CartIcon'
 import BurgerMenu from './BurgerMenu'
 import MenuModal from './MenuModal';
-
 import { useState } from 'react';
 
-export default function NavigationMenu() {
-    const [selectedOpen, setSelectedOpen] = useState(false);
 
-    const open = () => {
-        setSelectedOpen(!selectedOpen)
-        console.log('open');
+
+export default function NavigationMenu() {
+    const [isOpenMenu, setIsOpenMenu] = useState(false)
+
+    const handleClick = () => {
+        setIsOpenMenu(!isOpenMenu);
     }
+
+    
   return(
     <>
       <div className='flex gap-4 place-content-between items-center p-4'>
-        <div onClick={open} className="logo flex items-center gap-4 ">
-          {selectedOpen?<MenuModal/>: <BurgerMenu />}
+        <div  className="logo flex items-center gap-4 ">
+          <BurgerMenu onClick={handleClick}/>
           <a  className='uppercase' href=""><img src={logo} alt=""  /></a>
         </div>
         
@@ -26,6 +28,7 @@ export default function NavigationMenu() {
             <li><img className='w-7'src="\assets\productImages\image-avatar.png" alt="" /></li>
           </ul>
         </div>
+        {isOpenMenu && <MenuModal isActive={isOpenMenu} onClose={handleClick}/>}
 
       </div>
     </>
--- src/MenuModal.jsx
+++ src/MenuModal.jsx
@@ -1,13 +1,17 @@
 
+import { useState } from "react";
 import ExitIcon from "./ExitIcon";
 
-export default function MenuModal() {
-
+export default function MenuModal({ isActive, onClose }) {
+    
+    const clickOpen = () =>{
+        console.log('click en exit');
+    }
     return(
-        <div className="ventana-modal fixed top-0 backdrop-blur-lg w-full h-full z-10 left-0">
+        <div className={`ventana-modal fixed top-0 backdrop-blur-lg w-full h-full z-10 left-0`}>
             <div className="menu-modal bg-white w-1/2 h-screen p-4 flex flex-col gap-8">
-                <div className="exit">
-                    <ExitIcon/>
+                <div className="exit" >
+                    <ExitIcon onClick={onClose}/>
                 </div>
                 <div className="menu">
                 <ul className="flex flex-col gap-4">
